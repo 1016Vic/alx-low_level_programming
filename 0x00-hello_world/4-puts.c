3aeb8ce5e48282b24afc1a800a6948fafdb81dfe
1016Vic <victoroluwaseun25@gmail.com>
2022-12-11 15:24:26 -0800

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>  
+#include <stdio.h>
+/**
+ * main - Entry
+ * Descprition: 'How to use put'
+ * return: Always 0
+ */
 int main(void)
-	{
-puts("\"Programming is like building a multi lingual puzzle);                                                            return (0);
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
