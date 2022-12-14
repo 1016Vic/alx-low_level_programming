a58096274528615270f39e9cf211644f2b42f625
1016Vic <victoroluwaseun25@gmail.com>
2022-12-14 15:59:03 -0800

    task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write tge chatacter C to stdout
+ * @c: the charcaters to print
+ *
+ * Return: on Success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &C, 1));
+}
